1fcd923dcede800bfaa11e2bf7c3d2c0cd8cd28a
Serdar Ashirov <webapis@users.noreply.github.com>
2023-04-22 21:13:27 +0500

    1.0.2

--- utils/fetchRemoteImagesInParallel.js
+++ utils/fetchRemoteImagesInParallel.js
@@ -12,6 +12,13 @@ async function prefetchImage() {
 
     const limit = plimit(20);
 
+    const { downloadCollection } = require('./uploadCollection')
+    const genders = [{ gender: 'kadin', gender1: 'kadin' }, { gender: 'erkek', gender1: 'erkek' }, { gender: 'kcocuk', gender1: 'kiz-cocuk' }, { gender: 'ecocuk', gender1: 'erkek-cocuk' }]
+    console.log('process.env.marka', process.env.marka)
+    for (let g of genders) {
+        const { gender, gender1 } = g
+        await downloadCollection(gender, gender1, process.env.marka)
+    }
     let filePaths = []
 
     walkSync(path.join(process.cwd(), `erkek/unzipped-data`), async (filepath) => {
